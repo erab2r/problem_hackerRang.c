d7abcdf5002dbc6608eb4d1cc780d541549a9bc5
erab2r <shkera213@gmail.com>
2025-05-23 21:33:15 +0600

    3rd

---
+++ Almost_palindrome.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int t;
+    scanf("%d", &t); 
+    for (int i = 0; i < t; i++) {
+        char S[1001];
+        scanf("%s", S);  
+        int freq[26] = {0};          
+        int len = strlen(S);        
+        for (int i = 0;i<len;i++) {
+            freq[S[i]-'a']++;
+        }
+        int odd_count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] % 2 == 1) {
+                odd_count++;
+            }
+        }
+        int result;
+        if (odd_count > 0) {
+            result = odd_count - 1;
+        } else {
+            result = 0;
+        }
+        printf("%d\n", result);
+    }
+
+    return 0;
+}
